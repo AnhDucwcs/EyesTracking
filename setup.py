1e49f288067da91d5df8cbb25609abe788bffd36
AnhDucwcs <149997564+AnhDucwcs@users.noreply.github.com>
2025-11-04 19:44:32 +0700

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='src',   # hoặc 'eyes_tracking' nếu muốn tên gọn
-    version='0.0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-)
